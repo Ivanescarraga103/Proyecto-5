186e7b52c933be7819a71bf8acb1bba9f1aa19de
Ivanescarraga103 <elproivan002@gmail.com>
2023-09-23 08:52:58 -0500

    primer commit

---
+++ proyecto4/src/main/java/com/mycompany/Ivan/Proyecto4.java
@@ -0,0 +1,15 @@
+package com.mycompany.Ivan;
+import java.util.Scanner;
+public class Proyecto4 {
+public static void main(String[] args) {
+Scanner lista =new Scanner (System.in );
+float nn,nn4,p;
+System.out.print("Ingresa el primer numero");
+nn=lista.nextFloat();
+System.out.print("Ingresa segundo numero");
+nn4=lista.nextFloat();
+p=nn/nn4;
+System.out.print("El resultado de la division es "+p);
+
+    }
+}
